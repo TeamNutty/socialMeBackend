32c88d16cbdd0c4659c3e7e343c6e8e4126091eb
chonthicha sangcharean <kajichul.chon@gmail.com>
2021-10-25 10:42:38 +0700

    done  Change User Database

--- models/User.js
+++ models/User.js
@@ -26,6 +26,7 @@ module.exports = (sequelize, DataTypes) => {
       birthDate: DataTypes.DATEONLY,
       profilePicture: DataTypes.STRING,
       omiseId: DataTypes.STRING,
+      googleId: DataTypes.STRING,
     },
     {
       underscored: true,
