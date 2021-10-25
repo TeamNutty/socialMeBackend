0dd6eea576753ebe9e7f575fd8ed87d7973445e7
chonthicha sangcharean <kajichul.chon@gmail.com>
2021-10-25 17:47:44 +0700

    testOmiese

---
+++ controller/OmiseController.js
@@ -0,0 +1,36 @@
+const { OrderItemPost } = require('../models');
+
+const omise = require('omise')({
+  publicKey: process.env.OMISE_PUBLIC_KEY,
+  secretKey: process.env.OMISE_SECRET_KEY,
+});
+
+exports.buyPost = async (req, res, next) => {
+  const { price, tokenOmise, postId } = req.body;
+  console.log(req.body);
+  try {
+    //   username, amount, token
+    // const customer = await omise.customers.create({
+    //   email: req.user.email,
+    //   //   description: 'John Doe (id: 30)',
+    //   // card: token.id
+    //   card: 'tokn_test_5pma359qozi8v2z43lk',
+    // });
+
+    const charge = await omise.charges.create({
+      amount: price,
+      currency: 'thb',
+      //   customer: customer.id,
+      card: tokenOmise,
+      metadata: { userId: req.user.id, postId: postId },
+    });
+    if (charge.status === 'successful') {
+      await OrderItemPost.create({ userId: req.user.id, postId });
+      res.status(400).json({ message: 'done' });
+    } else {
+      return res.status(401).json({ message: 'buy fail' });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
