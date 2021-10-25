973082a5a41b73bb3615aa73dd4c19fb536594ff
NiratKashom <earthenly@gmail.com>
2021-10-25 17:08:58 +0700

    postController getMyPost, createPost message only

--- controller/PostController.js
+++ controller/PostController.js
@@ -1,14 +1,43 @@
 const { Post } = require('../models');
+const { User } = require('../models');
 
 exports.getAllMyPost = async (req, res, next) => {
   try {
-    const myPost = await Post.findAll({
+    const myPostList = await Post.findAll({
       where: {
-        id: req.user.id,
+        userId: req.user.id,
       },
+      include: [
+        {
+          model: User,
+          attributes: [
+            "firstName", "lastName",
+            "profilePicture",
+          ],
+          require: true
+        },
+      ]
     });
-    return res.status(200).json({ myPost });
+    return res.status(200).json({ myPostList });
   } catch (err) {
     next(err);
+  }
+};
+
+exports.createPost = async (req, res, next) => {
+  try {
+    const { userId, message, status } = req.body;
+    if (req.file) {
+      result = await uploadPromise(req.file.path);
+      fs.unlinkSync(req.file.path);
+    }
+    const newPost = await Post.create({
+      userId,
+      message,
+      status
+    });
+    res.status(200).json({ message: 'create post success' });
+  } catch (error) {
+    res.json({ message: error });
   }
 };
--- route/PostRoute.js
+++ route/PostRoute.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-
 const PostController = require('../controller/PostController');
 const { authenticate } = require('../controller/authController');
+const { upload } = require('../middleware/upload');
 
-router.get('/', authenticate, PostController.getAllMyPost);
+router.get('/mypost', authenticate, PostController.getAllMyPost);
+router.post('/', upload.single('picPost'), authenticate, PostController.createPost);
 
 module.exports = router;
