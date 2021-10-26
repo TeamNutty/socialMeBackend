bd3cb72ec5bcd45094515fb99bc0852432344ac9
TeamNutty <nattadolsmash@gmail.com>
2021-10-26 21:22:13 +0700

    team oldApi

--- route/UserRoute.js
+++ route/UserRoute.js
@@ -1,14 +1,14 @@
-const router = require('express').Router();
-const { authenticate } = require('../controller/authController');
-const UserController = require('../controller/UserController');
-const { upload } = require('../middleware/upload');
+const router = require("express").Router();
+const { authenticate } = require("../controller/authController");
+const UserController = require("../controller/UserController");
+const { upload } = require("../middleware/upload");
 
-router.get('/', UserController.getAllUser);
+router.get("/", UserController.getAllUser);
 
-router.get('/oneUser', authenticate, UserController.getOneUser);
-router.get('/otherUser', authenticate, UserController.getOtherUser);
+router.get("/oneUser", authenticate, UserController.getOneUser);
+router.get("/otherUser/:id", authenticate, UserController.getOtherUser);
 
-router.put('/userUpdate/:id', authenticate, upload.single('picture'), UserController.updateUser);
-router.put('/password/:id', authenticate, UserController.updateUserPassword);
+router.put("/userUpdate/:id", authenticate, upload.single("picture"), UserController.updateUser);
+router.put("/password/:id", authenticate, UserController.updateUserPassword);
 
 module.exports = router;
